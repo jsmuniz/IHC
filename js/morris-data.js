5876af85dd7731e7c47c54c243ed9c0e7c41c802
Jonathan Souza Muniz <jsmuniz7@outlook.com>
2015-12-09 19:38:44 -0200

    Admin Home e Relatórios
    Joao Paulo fez em 07/12

--- js/morris-data.js
+++ js/morris-data.js
@@ -1,65 +1,89 @@
 $(function() {
 
-    Morris.Area({
+    Morris.Bar({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '01/12',
+            pessoas: 2000
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '02/12',
+            pessoas: 2500
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '03/12',
+            pessoas: 2440
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '04/12',
+            pessoas: 1000
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '05/12',
+            pessoas: 700
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '05/12',
+            pessoas: 2200
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '06/12',
+            pessoas: 2500
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '07/12',
+            pessoas: 3000
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '08/12',
+            pessoas: 1000
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '09/12',
+            pessoas: 2000
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        parseTime: false,
+        ykeys: ['pessoas'],
+        labels: ['Pessoas'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
     });
+
+    Morris.Area({
+        element: 'morris-area-chart2',
+        data: [{
+            period: '01/12',
+            pessoas: 4565
+        }, {
+            period: '02/12',
+            pessoas: 4545
+        }, {
+            period: '03/12',
+            pessoas: 7535
+        }, {
+            period: '04/12',
+            pessoas: 3353
+        }, {
+            period: '05/12',
+            pessoas: 7383
+        }, {
+            period: '05/12',
+            pessoas: 3577
+        }, {
+            period: '06/12',
+            pessoas: 6841
+        }, {
+            period: '07/12',
+            pessoas: 5419
+        }, {
+            period: '08/12',
+            pessoas: 8152
+        }, {
+            period: '09/12',
+            pessoas: 5251
+        }],
+        xkey: 'period',
+        parseTime: false,
+        ykeys: ['pessoas'],
+        labels: ['Pessoas'],
+        pointSize: 2,
+        hideHover: 'auto',
+        resize: true
+    });
+
 
     Morris.Donut({
         element: 'morris-donut-chart',
