b6d3b31fc12233db39068b082c3e05dd0d342c23
Jonathan Souza Muniz <jsmuniz7@outlook.com>
2016-01-14 00:32:41 -0200

    Página Inicial do Usuário, Crítica da Entrada Cartão de Crédito, Correções Gerais

--- js/morris-data.js
+++ js/morris-data.js
@@ -4,7 +4,7 @@ $(function() {
         element: 'morris-area-chart',
         data: [{
             period: '01/12',
-            pessoas: 2000
+            pessoas: 10000
         }, {
             period: '02/12',
             pessoas: 2500
@@ -44,6 +44,48 @@ $(function() {
 
     Morris.Area({
         element: 'morris-area-chart2',
+        data: [{
+            period: '01/12',
+            pessoas: 4565
+        }, {
+            period: '02/12',
+            pessoas: 4545
+        }, {
+            period: '03/12',
+            pessoas: 7535
+        }, {
+            period: '04/12',
+            pessoas: 3353
+        }, {
+            period: '05/12',
+            pessoas: 7383
+        }, {
+            period: '05/12',
+            pessoas: 3577
+        }, {
+            period: '06/12',
+            pessoas: 6841
+        }, {
+            period: '07/12',
+            pessoas: 5419
+        }, {
+            period: '08/12',
+            pessoas: 8152
+        }, {
+            period: '09/12',
+            pessoas: 5251
+        }],
+        xkey: 'period',
+        parseTime: false,
+        ykeys: ['pessoas'],
+        labels: ['Pessoas'],
+        pointSize: 2,
+        hideHover: 'auto',
+        resize: true
+    });
+
+     Morris.Area({
+        element: 'morris-area-chart3',
         data: [{
             period: '01/12',
             pessoas: 4565
